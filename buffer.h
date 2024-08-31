1575dd65a40413cea00e489bbe009ada31373b7b
Mary Jane <maryj@greenwich_eduroam89-8.hampshire.edu>
2024-08-31 12:22:05 -0400

    init

--- buffer.h
+++ buffer.h
@@ -1,7 +1,4 @@
 /* no need for stdlib */
-/* no need for arena allocators either */
-
-// Use buffer(4096) for a page-sized buffer.
 
 typedef void *buffer_t; // Page-size
 #define buffer(x) ((buffer_t) &(char[x]){0})
